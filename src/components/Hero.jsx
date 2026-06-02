4c215b342b015e717872cb22f1ab7833ab5e0407
septiancahyocahyo <septiancahyo67@gmail.com>
2026-06-02 12:12:06 +0700

    fix: useRef unused

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { FiArrowDown, FiLinkedin, FiMail, FiPhone } from 'react-icons/fi';
 
